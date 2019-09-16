87c3a4cd72ce86c2546346f6b9aca2080f94cd02
João Pedro Silva de Queiroz <joaopedrosilvaqueiroz@gmail.com>
2019-09-16 00:18:22 -0300

    Implementação Classe dos Vídeos (WIP)

--- video/js/script.js
+++ video/js/script.js
@@ -1,14 +1,31 @@
 var myVideo1 = document.getElementById("video1"); 
 
-function playPause1() { 
-	if (myVideo1.paused){ 
-		myVideo1.play(); 
-  	}else{ 
-		myVideo1.pause(); 
- 	}
-}
+var btn_play = document.getElementById("playpause");
+var btn_stop = document.getElementById("btn_stop");
+var bar = document.getElementById("bar");
 
-function voltar1() {				
-	myVideo1.currentTime = 0;
-	myVideo1.pause(); 
-}
+myVideo1.onended = function() {
+    myVideo1.currentTime = 0;
+    btn_play.innerHTML = "Play";
+};
+
+btn_play.addEventListener("click", function () {
+    myVideo1.paused ? (btn_play.innerHTML = "Pause", myVideo1.play()) : (btn_play.innerHTML = "Play", myVideo1.pause())
+});
+
+btn_stop.addEventListener("click", function () {
+    myVideo1.currentTime = 0;
+    myVideo1.pause();
+    btn_play.innerHTML = "Play";
+});
+
+/*g.addEventListener("click", function (k) {
+    this.rect = g.getBoundingClientRect();
+    this.a = (k.clientX - this.rect.left) / g.clientWidth;
+    a.currentTime = a.duration * this.a
+});*/
+
+myVideo1.addEventListener("timeupdate", function () {
+    bar.style.width = myVideo1.currentTime / myVideo1.duration * 100 + "%";
+    myVideo1.ended && (btn_play.innerHTML = "Play")
+});
